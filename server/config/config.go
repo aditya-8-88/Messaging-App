9ac7f6eee0d56008c7993cf774ebab5fa86a8b04
Aditya Pratap Singh <adityapratapsingh12b05@gmail.com>
2026-02-24 06:30:24 +0000

    removed boiler code

--- server/config/config.go
+++ server/config/config.go
@@ -1,18 +1,5 @@
 package config
 
-import (
-    "log"
-    "os"
-    "github.com/joho/godotenv"
-)
-
 func Load() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-}
 
-func Get(key string) string {
-    return os.Getenv(key)
 }
